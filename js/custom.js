ef02773d6dbede7d8ac870b187460a8ebcd81a6a
Medard <rmedard5@gmail.com>
2017-11-28 16:35:04 +0100

    EC

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,3 @@
 (function ($) {
-
-    var isMobile = {
-        Android: function () {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function () {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function () {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function () {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function () {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function () {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-
-    if (isMobile.any() && window.innerHeight < window.innerWidth) {
-        $('div#views-bootstrap-products-page-products > div.media > div').addClass('col-xs-12')
-    }
-
     $('div#views-bootstrap-products-page-products > div.media').addClass('well well-sm');
 }(jQuery));
